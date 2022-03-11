dc93743d00643e369948d5757eb49d142eecc88c
Andrew Gary <andrew.gary91@gmail.com>
2022-03-11 01:31:51 -0600

    Finished MVP

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ server.use(express.json());
 
 const PORT = process.env.PORT || 9000;
 
-server.get('/', (req, res) => {
-    res.status(200).json([
-        {id: 1, username: 'User1', email: 'User1@email.com'},
-        {id: 2, username: 'User2', email: 'User2@email.com'},
-        {id: 3, username: 'User3', email: 'User3@email.com'},
-        {id: 4, username: 'User4', email: 'User4@email.com'}
-    ])
+const usersArray = [
+    {id: 1, username: 'User1', email: 'User1@email.com'},
+    {id: 2, username: 'User2', email: 'User2@email.com'},
+    {id: 3, username: 'User3', email: 'User3@email.com'},
+    {id: 4, username: 'User4', email: 'User4@email.com'}
+]
+
+server.get('/api/users', (req, res) => {
+    res.status(200).json(usersArray)
+})
+
+server.post('/api/register', (req, res) => {
+    if(req.body.username && req.body.password){
+        res.status(201).json(req.body);
+    }
+})
+
+server.post('/api/login', (req, res) => {
+    if(req.body.username && req.body.password){
+        res.status(201).json({ message: `Welcome back ${req.body.username}`})
+    }
 })
 
 server.listen(PORT, () => {
